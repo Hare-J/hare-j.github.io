ef83e0b1051b58c13e3ee1bc7d89d814cf75dd52
Hare-J <114368675+Hare-J@users.noreply.github.com>
2023-03-20 19:15:02 -0400

    Reformatted site to fit a one-page design

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+document.getElementById("btnAbout").addEventListener('click', function(){
+    document.getElementById("about-page").scrollIntoView({behavior: 'smooth', block: 'start'})
+})
+
+document.getElementById("btnPortfolio").addEventListener('click', function(){
+    document.getElementById("portfolio-page").scrollIntoView({behavior: 'smooth', block: 'start'})
+})
