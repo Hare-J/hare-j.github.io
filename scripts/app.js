4973951cb5b9877a485071224f42a1fc69b4f59c
Hare-J <114368675+Hare-J@users.noreply.github.com>
2023-03-24 11:51:59 -0400

    added comments

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,9 @@
+// smooth scroll down to About Me section of the page
 document.getElementById("btnAbout").addEventListener('click', function(){
     document.getElementById("about-page").scrollIntoView({behavior: 'smooth', block: 'start'})
 })
 
+// smooth scroll down to Portfolio section of page
 document.getElementById("btnPortfolio").addEventListener('click', function(){
     document.getElementById("portfolio-page").scrollIntoView({behavior: 'smooth', block: 'start'})
 })
