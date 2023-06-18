707e34b2c8a4198b39140df18b5456cb79eded20
Hare-J <114368675+Hare-J@users.noreply.github.com>
2023-06-17 21:52:55 -0400

    added skills section

--- scripts/app.js
+++ scripts/app.js
@@ -55,6 +55,11 @@ document.getElementById("btnAbout").addEventListener('click', function(){
     document.getElementById("about-page").scrollIntoView({behavior: 'smooth', block: 'start'});
 })
 
+// smooth scroll down to Skills section of page
+document.getElementById("btnSkills").addEventListener('click', function(){
+    document.getElementById("skills-page").scrollIntoView({behavior: 'smooth', block: 'start'});
+})
+
 // smooth scroll down to Portfolio section of page
 document.getElementById("btnPortfolio").addEventListener('click', function(){
     document.getElementById("portfolio-page").scrollIntoView({behavior: 'smooth', block: 'start'});
